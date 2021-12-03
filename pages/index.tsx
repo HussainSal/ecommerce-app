46a070698d26d0421c7118ea121d8d6bd7283aba
unknown <salmanmian54321@gmail.com>
2021-12-03 12:07:44 +0530

    tollssetted

---
+++ pages/index.tsx
@@ -0,0 +1,23 @@
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+/*
+
+# If everything passes... Now we can commit
+echo '🤔🤔🤔🤔... Alright.... Code looks good to me... Trying to build now. 🤔🤔🤔🤔'
+
+npm run build ||
+(
+    echo '❌👷🔨❌ Better call Bob... Because your build failed ❌👷🔨❌
+            Next build failed: View the errors above to see why. 
+    '
+    false;
+)
+*/
